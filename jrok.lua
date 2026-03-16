3ecea87296774726d40390b21c95d280e7fdfc34
Alvin Dong <alvindong9913@gmail.com>
2026-03-16 11:09:23 -0500

    prompt engineering

--- jrok.lua
+++ jrok.lua
@@ -2,11 +2,233 @@ SMODS.Atlas({
 	key = "al_jokers",
 	path = "al_jokers.png",
 	px = 71,
-	py = 95
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "modicon",
+	path = "jrok.png",
+	px = 34,
+	py = 34,
 })
 
 for k, c in pairs(G.P_CENTERS) do
 	if c.set == "Joker" and not c.original_mod then
-		SMODS.Joker:take_ownership(string.sub(k, 3), {atlas = "al_jokers"}, true)
+		SMODS.Joker:take_ownership(string.sub(k, 3), { atlas = "al_jokers", pixel_size = { w = 71, h = 95 } }, true)
+	end
+end
+
+local init_loc_hook = init_localization
+function init_localization()
+	for k, c in pairs(G.P_CENTERS) do
+		if c.set == "Joker" and not c.original_mod then
+			local text = G.localization.descriptions.Joker[k].text
+			local new_text = {}
+			for _, line in ipairs(text) do
+				local res = line
+				if string.find(line, "(({.*}).*)[aA][iI]") then
+					res = string.gsub(line, "(({.*}).*)[aA][iI]", "%1{C:attention}AI%2")
+				else
+					res = string.gsub(line, "[aA][iI]", "{C:attention}AI{}")
+				end
+				new_text[#new_text + 1] = res
+			end
+			G.localization.descriptions.Joker[k].text = new_text
+		end
+	end
+	init_loc_hook()
+end
+
+JROK = {}
+JROK.prompt_text = ""
+
+function G.FUNCS.jrok_prompt_menu(e)
+	local uidef = {
+		n = G.UIT.ROOT,
+		config = {
+			align = "cm",
+			minw = G.ROOM.T.w * 5,
+			minh = G.ROOM.T.h * 5,
+			padding = 0.1,
+			r = 0.1,
+			colour = { G.C.GREY[1], G.C.GREY[2], G.C.GREY[3], 0.7 },
+		},
+		nodes = {
+			{
+				n = G.UIT.R,
+				config = { r = 0.1, colour = G.C.JOKER_GREY, padding = 0.05, align = "cm" },
+				nodes = {
+					{
+						n = G.UIT.C,
+						config = { colour = G.C.L_BLACK, r = 0.1, padding = 0.2, align = "cm" },
+						nodes = {
+							{
+								n = G.UIT.R,
+								config = { align = "cm" },
+								nodes = {
+									{
+										n = G.UIT.C,
+										config = { align = "cm", minh = 4 },
+										nodes = {
+											{
+												n = G.UIT.R,
+												config = { align = "cm", padding = 0.1 },
+												nodes = {
+													{
+														n = G.UIT.O,
+														config = {
+															object = SMODS.create_sprite(
+																0,
+																0,
+																1,
+																1,
+																"jrok_modicon",
+																{ x = 0, y = 0 }
+															),
+														},
+													},
+													{
+														n = G.UIT.B,
+														config = { w = 0.1, h = 0.1 },
+													},
+													{
+														n = G.UIT.T,
+														config = {
+															text = localize("k_jrok"),
+															scale = 0.8,
+															colour = G.C.UI.TEXT_LIGHT,
+														},
+													},
+												},
+											},
+											{
+												n = G.UIT.R,
+												config = { align = "cm", padding = 0.1 },
+												nodes = {
+													{
+														n = G.UIT.T,
+														config = {
+															text = localize("k_jrok_prompt"),
+															scale = 0.7,
+															colour = G.C.UI.TEXT_LIGHT,
+														},
+													},
+												},
+											},
+											{
+												n = G.UIT.R,
+												config = { minh = 0.2 },
+											},
+											{
+												n = G.UIT.R,
+												config = { padding = 0.1, align = "cm" },
+												nodes = {
+													create_text_input({
+														prompt_text = localize("k_jrok_text_prompt"),
+														ref_table = JROK,
+														ref_value = "prompt_text",
+														max_length = 500,
+														w = 11,
+														extended_corpus = true,
+													}),
+													{
+														n = G.UIT.C,
+														config = {
+															button = "start_setup_run_from_jrok",
+															ref_element = e,
+															colour = G.C.BLUE,
+															minh = 0.6,
+															minw = 1.8,
+															padding = 0.05,
+															r = 0.1,
+															hover = true,
+															shadow = true,
+															align = "cm",
+															func = "can_start_jrok",
+														},
+														nodes = {
+															{
+																n = G.UIT.T,
+																config = {
+																	text = localize("k_jrok_start"),
+																	scale = 0.4,
+																	colour = G.C.UI.TEXT_LIGHT,
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+							{
+								n = G.UIT.R,
+								config = {
+									id = "overlay_menu_back_button",
+									align = "cm",
+									minw = 2.5,
+									padding = 0.1,
+									r = 0.1,
+									hover = true,
+									colour = G.C.ORANGE,
+									button = "exit_overlay_menu",
+									shadow = true,
+									focus_args = { nav = "wide", button = "b" },
+								},
+								nodes = {
+									{
+										n = G.UIT.R,
+										config = { align = "cm", padding = 0, no_fill = true },
+										nodes = {
+											{
+												n = G.UIT.T,
+												config = {
+													text = localize("b_back"),
+													scale = 0.5,
+													colour = G.C.UI.TEXT_LIGHT,
+													shadow = true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	G.FUNCS.overlay_menu({
+		definition = uidef,
+		config = {
+			--no_esc = true,
+		},
+	})
+end
+
+function G.FUNCS.start_setup_run_from_jrok(e)
+	G.FUNCS.start_setup_run(e.config.ref_element)
+end
+
+function G.FUNCS.can_start_run(e)
+	if not G.GAME.viewed_back.effect.center.unlocked then
+		e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+		e.config.button = nil
+	else
+		e.config.colour = G.C.BLUE
+		e.config.button = G.SETTINGS.current_setup == "Continue" and "start_setup_run" or "jrok_prompt_menu"
+	end
+end
+
+function G.FUNCS.can_start_jrok(e)
+	if JROK.prompt_text ~= "" then
+		e.config.colour = G.C.BLUE
+		e.config.button = "start_setup_run_from_jrok"
+	else
+		e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+		e.config.button = nil
 	end
 end
---
+++ localization/en-us.lua
@@ -0,0 +1,16 @@
+return {
+	misc = {
+		poker_hands = {
+			Straight = "StrAIght",
+			Pair = "PAIr",
+			["Straight Flush"] = "StrAIght Flush",
+			["Two Pair"] = "Two PAIr",
+		},
+        dictionary = {
+            k_jrok_prompt = "Generate your own Balatro content",
+            k_jrok_text_prompt = "What's your idea?",
+            k_jrok_start = "Start Run",
+            k_jrok = "JROK",
+        }
+	},
+}
