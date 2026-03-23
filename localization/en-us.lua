a998f13659506f11c0ebb9d97b716584af39b8f1
Alvin Dong <alvindong9913@gmail.com>
2026-03-22 19:25:24 -0500

    jrok shit

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,4 +1,26 @@
 return {
+	descriptions = {
+		Joker = {
+			j_tboj_monstro_lung = {
+				name = "Monstro's Lung",
+				text = {
+					"Randomly add {C:attention}#1#{} to {C:attention}#2#{}",
+					"cards to your {C:attention}played hand",
+					"before determining the {C:attention}poker hand{},",
+					"then {C:attention}destroy{} them after scoring",
+				},
+			},
+			j_jrok_dream_catcher = {
+				name = "Dream Catcher",
+				text = {
+					"The next {C:attention}Boss Blind{}",
+					"is {C:attention}#1#",
+					"{s:0.8}Prediction might change",
+					"{s:0.8}based on your actions",
+				},
+			},
+		},
+	},
 	misc = {
 		poker_hands = {
 			Straight = "StrAIght",
@@ -6,16 +28,16 @@ return {
 			["Straight Flush"] = "StrAIght Flush",
 			["Two Pair"] = "Two PAIr",
 		},
-        dictionary = {
-            k_jrok_prompt = "Generate your own Balatro content",
+		dictionary = {
+			k_jrok_prompt = "Generate your own Balatro content",
 			k_jrok_prompt2 = "How may I help you?",
-            k_jrok_text_prompt = "What's your idea?",
+			k_jrok_text_prompt = "What's your idea?",
 			k_jrok_text_prompt2 = "Ask anything",
-            k_jrok_start = "Start Run",
+			k_jrok_start = "Start Run",
 			k_jrok_arrow = ">",
-            k_jrok = "JROK",
+			k_jrok = "JROK",
 			k_jrok_powered = "(Powered",
 			k_by_jrok = "by JROK)",
-        }
+		},
 	},
 }
